e4b7e6436775b474380afba3a06dd51156e68dd5
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-10-24 15:32:01 -0500

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,28 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		ssr: {
+			external: [
+				"url",
+				"crypto",
+				"http",
+				"https",
+				"fs",
+				"http2",
+				"events",
+				"zlib",
+				"node:stream",
+				"node:util",
+				"os",
+				"path",
+				"node:events",
+				"util",
+				"stream",
+				"buffer",
+			],
+		},
+	},
 	output: "server",
 	adapter: cloudflare(),
 });
